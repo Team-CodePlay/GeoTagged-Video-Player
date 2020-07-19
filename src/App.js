ad915acf5eb8de4b66b0130bf8215751e0639079
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-07-19 23:46:03 +0530

    Add react video player

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import 'video-react/dist/video-react.css';
+import { Player } from 'video-react'
 
-function App() {
+const App = () => {
   return (
-    <div>Hello World</div>
+    <div>
+      <Player>
+        <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+      </Player>
+    </div>  
   );
 }
 
